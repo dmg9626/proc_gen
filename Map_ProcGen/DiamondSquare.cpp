7d7e88e17be0e8fbf4ab809ea3977669f85f06cc
Drew Graham <dmg9626@gmail.com>
2020-03-12 15:32:14 -0400

    got Diamond step working

--- Map_ProcGen/DiamondSquare.cpp
+++ Map_ProcGen/DiamondSquare.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include "DiamondSquare.h"
 
-float** DiamondSquare::GenerateHeightMap(int n)
+float** DiamondSquare::GenerateHeightMap(const int n)
 {
 	if (n <= 1)
 		return nullptr;
-
-	// TODO: pass in srand() seed to method and set it here
 
 	// Initialize empty heightmap
 	int size = pow(2, n) + 1;
@@ -21,13 +19,56 @@ float** DiamondSquare::GenerateHeightMap(int n)
 	bottomRow[0] = rand01();
 	bottomRow[size - 1] = rand01();
 	
-	// TODO: generate heightmap here
+	for (int i = 0; i < n; i++) {
+		// Populate values across map in increasingly granular steps
+		int step = pow(2, n-i) / 2;
+		printf("Step size: %d\n", step);
+
+		// SQUARE
+		Square(size, step, rows);
+
+		// DIAMOND
+		Diamond(size, step, rows);
+	}
 
 	// Return generated heightmap array
 	return rows;
 }
 
-float** DiamondSquare::InitializeFloatArray(int size)
+void DiamondSquare::Diamond(const int size, const int step, float** heightmap)
+{
+	// TODO: halve noise after each iteration
+	float noise = .1f;
+
+	// Iterate over rows
+	for (int y = step; y < size-1; y += step) 
+	{
+		// Iterate over items in row
+		float* row = heightmap[y];
+		for (int x = step; x < size - 1; x += step) 
+		{
+			// Get average of diagonally-neighboring values at step distance
+			float topLeft = heightmap[y - step][x - step];
+			float topRight = heightmap[y - step][x + step];
+			float bottomLeft = heightmap[y + step][x - step];
+			float bottomRight = heightmap[y + step][x + step];
+
+			float avg = (topLeft + topRight + bottomLeft + bottomRight) / 4;
+			//printf("neighbors: %.2f, %.2f, %.2f, %.2f\n", topLeft, topRight, bottomLeft, bottomRight);
+			//printf("average: %.2f\n", avg);
+
+			// Set tile value to avg + noise
+			row[x] = avg + (avg * noise);
+		}
+	}
+}
+
+void DiamondSquare::Square(const int size, const int step, float** heightmap)
+{
+
+}
+
+float** DiamondSquare::InitializeFloatArray(const int size)
 {
 	printf("Generating map of size %d\n\n", size);
 
--- Map_ProcGen/DiamondSquare.h
+++ Map_ProcGen/DiamondSquare.h
@@ -5,17 +5,17 @@ class DiamondSquare
 {
 public:
 	/* Returns a 2D float array representing a heightmap of size 2^(n) + 1 */
-	float** GenerateHeightMap(int n);
+	float** GenerateHeightMap(const int n);
 
 private:
 	/* Performs diamond step of algorithm */
-	void Diamond(int size, int step, float** heightmap);
+	void Diamond(const int size, const int step, float** heightmap);
 	
 	/* Performs square step of algorithm */
-	void Square(int size, int step, float** heightmap);
+	void Square(const int size, const int step, float** heightmap);
 
 	/* Returns a properly-sized float array of zeroes */
-	float** InitializeFloatArray(int size);
+	float** InitializeFloatArray(const int size);
 
 	/* Returns random float value between 0 and 1 (inclusive) */
 	float rand01() {
